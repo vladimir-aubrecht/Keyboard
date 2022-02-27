061d1080f5118cc7530f9e5d067a301a695d6f0b
Vladimír Aubrecht <vladimir.aubrecht@me.com>
2022-02-28 00:25:18 +0100

    Firmware in dotnet [WIP]

---
+++ firmware/Custom/DotnetKeyboard/DotnetKeyboard/DotnetKeyboard/Program.cs
@@ -0,0 +1,20 @@
+﻿using Meadow;
+
+using System.Threading;
+
+namespace DotnetKeyboard
+{
+    internal class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "--exitOnDebug") return;
+
+            // instantiate and run new meadow app
+            app = new MeadowApp();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
