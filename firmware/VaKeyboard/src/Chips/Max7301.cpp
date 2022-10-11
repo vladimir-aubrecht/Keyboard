07c8d124473f96632dc64ae4c8a7c80c27d1c3c9
Vladimir Aubrecht <vladimir.aubrecht@me.com>
2022-10-11 23:56:00 +0200

    Added feather32u4 specific optimization for Max7301.

--- firmware/VaKeyboard/src/Chips/Max7301.cpp
+++ firmware/VaKeyboard/src/Chips/Max7301.cpp
@@ -25,11 +25,25 @@ byte Max7301::transferByte(byte data_out)
 
     for(current_bit = 0; current_bit < 8; current_bit++)
     {
+        #ifdef FEATHER32U4
+        PORTB &= B11011111;
+        #else
         digitalWrite(this->sclkPin, LOW);
+        #endif
+
+        //uint8_t readBit = (PINC >> PC7) & 1;
         result = (result << 1) | digitalRead(this->misoPin);
-        digitalWrite(this->mosiPin, data_out & 0x80);
+
+        PORTB &= B10111111;
+        PORTB |= (data_out & 0x80) >> 1; // D10 is bit 6 MOSI, D9 is bit 5 CLK, D11 is bit 7 CS. Significantly faster version of: digitalWrite(this->mosiPin, data_out & 0x80);
+
         data_out <<= 1;
+        
+        #ifdef FEATHER32U4
+        PORTB |= B00100000;
+        #else
         digitalWrite(this->sclkPin, HIGH);
+        #endif
     }
 
     return result;
@@ -37,14 +51,22 @@ byte Max7301::transferByte(byte data_out)
 
 uint8_t Max7301::transferWord(uint8_t cmdByte, uint8_t dataByte)
 {
-    digitalWrite(this->sclkPin, LOW);
+    #ifdef FEATHER32U4
+    PORTB &= B01111111;
+    #else
     digitalWrite(this->csPin, LOW);
+    #endif
     
     transferByte(cmdByte);
     uint8_t data = transferByte(dataByte);
     
+    #ifdef FEATHER32U4
+    PORTB |= B10000000; 
+    PORTB &= B11011111;
+    #else
     digitalWrite(this->csPin, HIGH);
     digitalWrite(this->sclkPin, LOW);
+    #endif
 
     return data;
 }
